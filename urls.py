39c578f33d9b9b463121b568f07fa92a386a7413
razmikaghavelyan <98036949+razmikaghavelyan@users.noreply.github.com>
2022-02-23 19:27:19 +0400

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django_app import views
-
-urlpatterns = [
-    path('', views.home),
-    path('daytime/', views.daytime),
-    path('intro/', views.introduction),
-    path('task1/', views.square_keys),
-]
